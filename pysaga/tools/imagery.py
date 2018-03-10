900150d4217b183aac5bc95fd6ff534876403623
Saul Arciniega <zaul_ae@hotmail.com>
2018-03-09 19:05:46 -0600

    Starting with imagery

---
+++ pysaga/tools/imagery.py
@@ -0,0 +1,59 @@
+"""
+SAGA GIS algorithm provider
+Grids tools:
+    grid_analysis
+    grid_calculus
+    grid_filter
+    grid_gridding
+    grid_spline
+    grid_tools
+
+
+Author:
+Saul Arciniega Esparza
+zaul.ae@gmail.com
+Institute of Engineering of UNAM
+Mexico City
+"""
+
+# env is the provider class
+import sys as _sys
+import os as _os
+import numpy as _np
+import pandas as _pd
+import itertools as _itertools
+
+import projection as _projection
+from tables import get_attribute_table as _get_attribute_table
+
+
+_ERROR_TEXT = ('Error running "{}()", please check the error file: {}')
+
+
+# ==============================================================================
+# Library: imagery_classification
+# ==============================================================================
+
+
+# ==============================================================================
+# Library: imagery_maxent
+# ==============================================================================
+
+
+# ==============================================================================
+# Library: imagery_opencv
+# ==============================================================================
+
+
+
+# ==============================================================================
+# Library: imagery_tools
+# ==============================================================================
+
+
+# ==============================================================================
+# Library: imagery_vigra
+# ==============================================================================
+
+
+
